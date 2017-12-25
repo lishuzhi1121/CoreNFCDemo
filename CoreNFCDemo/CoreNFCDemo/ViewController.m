d7f47d2e87897566d4f4f0f7a04c97d99dbf0711
Sands_Lee <sands_lee@Sands-iMac.local>
2017-12-25 09:53:11 +0800

    demo

--- CoreNFCDemo/CoreNFCDemo/ViewController.m
+++ CoreNFCDemo/CoreNFCDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <CoreNFC/CoreNFC.h>
 
-@interface ViewController ()
+@interface ViewController () <NFCNDEFReaderSessionDelegate>
+
+@property (nonatomic, strong) NFCNDEFReaderSession *ndefSession;
 
 @end
 
@@ -16,14 +19,105 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.ndefSession = [[NFCNDEFReaderSession alloc] initWithDelegate:self queue:dispatch_queue_create(NULL, DISPATCH_QUEUE_CONCURRENT) invalidateAfterFirstRead:NO];
+    
+    [self.ndefSession beginSession];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.ndefSession = [[NFCNDEFReaderSession alloc] initWithDelegate:self queue:dispatch_queue_create(NULL, DISPATCH_QUEUE_CONCURRENT) invalidateAfterFirstRead:NO];
+    [self.ndefSession beginSession];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - NFCNDEFReaderSessionDelegate
+
+/*!
+ * @method readerSession:didInvalidateWithError:
+ *
+ * @param session   The session object that is invalidated.
+ * @param error     The error indicates the invalidation reason.
+ *
+ * @discussion      Gets called when a session becomes invalid.  At this point the client is expected to discard
+ *                  the returned session object.
+ */
+- (void)readerSession:(NFCNDEFReaderSession *)session didInvalidateWithError:(NSError *)error {
+    if (error)
+    {
+        NSLog(@"Error: %@", error);
+    }
+    if (session)
+    {
+        [session invalidateSession];
+    }
 }
 
+
+/*!
+ * @method readerSession:didDetectNDEFs:
+ *
+ * @param session   The session object used for tag detection.
+ * @param messages  Array of @link NFCNDEFMessage @link/ objects. The order of the discovery on the tag is maintained.
+ *
+ * @discussion      Gets called when the reader detects NFC tag(s) with NDEF messages in the polling sequence.  Polling
+ *                  is automatically restarted once the detected tag is removed from the reader's read range.
+ */
+- (void)readerSession:(NFCNDEFReaderSession *)session didDetectNDEFs:(NSArray<NFCNDEFMessage *> *)messages {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        for (NFCNDEFMessage *message in messages) {
+            NSLog(@"NFCNDEFMessage: %@", message);
+            for (NFCNDEFPayload *payload in message.records) {
+                NSLog(@"NFCNDEFPayload data:%@",payload.payload);
+                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"NFCNDEFPayload"
+                                                                    message:[[NSString alloc] initWithData:payload.payload encoding:NSUTF8StringEncoding]
+                                                                   delegate:nil
+                                                          cancelButtonTitle:@"OK"
+                                                          otherButtonTitles:nil, nil];
+                [alertView show];
+            }
+        }
+    });
+}
+
+#pragma mark - NFCReaderSessionDelegate
+
+/*!
+ * @method readerSessionDidBecomeActive:
+ *
+ * @param session   The session object in the active state.
+ *
+ * @discussion      Gets called when the NFC reader session has become active. RF is enabled and reader is scanning for tags.
+ *                  The @link readerSession:didDetectTags: @link/ will be called when a tag is detected.
+ */
+- (void)readerSessionDidBecomeActive:(NFCReaderSession *)session {
+    
+}
+
+
+/*!
+ * @method readerSession:didDetectTags:
+ *
+ * @param session   The session object used for tag detection.
+ * @param tags      Array of @link NFCTag @link/ objects.
+ *
+ * @discussion      Gets called when the reader detects NFC tag(s) in the polling sequence.
+ */
+- (void)readerSession:(NFCReaderSession *)session didDetectTags:(NSArray<__kindof id<NFCTag>> *)tags {
+    
+}
+
+/*!
+ * @method readerSession:didInvalidateWithError:
+ *
+ * @param session   The session object that is invalidated.
+ * @param error     The error indicates the invalidation reason.
+ *
+ * @discussion      Gets called when a session becomes invalid.  At this point the client is expected to discard
+ *                  the returned session object.
+ */
+//- (void)readerSession:(NFCReaderSession *)session didInvalidateWithError:(NSError *)error {
+//
+//}
 
 @end
